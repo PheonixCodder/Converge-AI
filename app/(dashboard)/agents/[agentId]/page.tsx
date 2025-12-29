2d3efd56920b5cd8bb9726d1812eb725e6d1a606
Abdulahad <abdulahadismail.dev@gmail.com>
2025-12-29 22:51:10 +0500

    Create Agent Page

---
+++ app/(dashboard)/agents/[agentId]/page.tsx
@@ -0,0 +1,23 @@
+import { AgentIdView } from "@/modules/agents/ui/views/agent-id-view";
+import { trpc } from "@/trpc/server";
+import { SuspenseErrorBoundary } from "@/components/suspense-error-boundary";
+import { prefetch } from "@/server/prefetch";
+
+type AgentPageProps = {
+  params: Promise<{ agentId: string }>;
+};
+
+const AgentPage = async ({ params }: AgentPageProps) => {
+  const { agentId } = await params;
+  const queryClient = prefetch(
+    trpc.agents.getById.queryOptions({ id: agentId })
+  );
+
+  return (
+    <SuspenseErrorBoundary queryClient={queryClient} title="agent">
+      <AgentIdView agentId={agentId} />
+    </SuspenseErrorBoundary>
+  );
+};
+
+export default AgentPage;
