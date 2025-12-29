b16765fe36bded9ab1245acfbe4cbd0a20d4e4a9
Abdulahad <abdulahadismail.dev@gmail.com>
2025-12-29 22:52:36 +0500

    Create Meeting Page

---
+++ app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -0,0 +1,23 @@
+import { SuspenseErrorBoundary } from "@/components/suspense-error-boundary";
+import { MeetingIdView } from "@/modules/meetings/ui/views/meeting-id-view";
+import { prefetch } from "@/server/prefetch";
+import { trpc } from "@/trpc/server";
+
+type MeetingPageProps = {
+  params: Promise<{ meetingId: string }>;
+};
+
+const MeetingPage = async ({ params }: MeetingPageProps) => {
+  const { meetingId } = await params;
+  const queryClient = prefetch(
+    trpc.meetings.getById.queryOptions({ id: meetingId })
+  );
+
+  return (
+    <SuspenseErrorBoundary queryClient={queryClient} title="meeting">
+      <MeetingIdView meetingId={meetingId} />
+    </SuspenseErrorBoundary>
+  );
+};
+
+export default MeetingPage;
